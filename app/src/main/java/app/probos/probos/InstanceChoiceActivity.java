f4fce715125cd214b27ab669b32041755e0eb9b6
Steven Vaughan <svaughan@purdue.edu>
2019-02-11 21:19:02 -0500

    Implemented basic OAuth functionality

--- app/src/main/java/app/probos/probos/InstanceChoiceActivity.java
+++ app/src/main/java/app/probos/probos/InstanceChoiceActivity.java
@@ -3,6 +3,8 @@ package app.probos.probos;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -24,6 +26,8 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -32,11 +36,20 @@ import android.widget.TextView;
 
 
 import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+import com.sys1yagi.mastodon4j.rx.*;
 import com.sys1yagi.mastodon4j.*;
+import com.sys1yagi.mastodon4j.api.Pageable;
+import com.sys1yagi.mastodon4j.api.Range;
 import com.sys1yagi.mastodon4j.api.Scope;
+import com.sys1yagi.mastodon4j.api.entity.Status;
+import com.sys1yagi.mastodon4j.api.entity.auth.AccessToken;
 import com.sys1yagi.mastodon4j.api.entity.auth.AppRegistration;
 import com.sys1yagi.mastodon4j.api.exception.Mastodon4jRequestException;
 import com.sys1yagi.mastodon4j.api.method.Apps;
+import com.sys1yagi.mastodon4j.api.method.Timelines;
+
+import org.w3c.dom.Text;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -68,7 +81,7 @@ public class InstanceChoiceActivity extends AppCompatActivity implements LoaderC
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
-    private UserLoginTask mAuthTask = null;
+    //private UserLoginTask mAuthTask = null;
 
     // UI references.
     private AutoCompleteTextView mEmailView;
@@ -84,8 +97,8 @@ public class InstanceChoiceActivity extends AppCompatActivity implements LoaderC
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
 
-        mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        //mPasswordView = (EditText) findViewById(R.id.password);
+        /*mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
                 if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
@@ -94,13 +107,25 @@ public class InstanceChoiceActivity extends AppCompatActivity implements LoaderC
                 }
                 return false;
             }
-        });
+        });*/
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        Button mEmailSignInButton = (Button) findViewById(R.id.getauth_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 attemptLogin();
+            }
+        });
+
+        Button mSignInButton = (Button) findViewById(R.id.signin_button);
+        mSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    actualSignIn();
+                } catch (Exception e) {
+                    throw new IndexOutOfBoundsException();
+                }
             }
         });
 
@@ -157,34 +182,61 @@ public class InstanceChoiceActivity extends AppCompatActivity implements LoaderC
      * If there are form errors (invalid email, missing fields, etc.), the
      * errors are presented and no actual login attempt is made.
      */
+    MastodonClient client;
+    MastodonClient authClient;
+    AppRegistration registration;
+    String instanceName;
+    String authCode;
+    Apps apps;
+    String clientId;
+    String clientSecret;
+
+    private WebView mWebview ;
+
     private void attemptLogin() {
         //if (mAuthTask != null) {
           //  return;
         //}
-
+        TextView login = findViewById(R.id.email);
+        instanceName = login.getText().toString();
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                MastodonClient client = new MastodonClient.Builder("cyber-anarchy.net", new OkHttpClient.Builder(), new Gson()).build();
-                Apps apps = new Apps(client);
+                client = new MastodonClient.Builder(instanceName, new OkHttpClient.Builder(), new Gson()).build();
+                apps = new Apps(client);
                 try {
-                    AppRegistration registration = apps.createApp(
-                            "probos-app",
+                    registration = apps.createApp(
+                            "Probos",
                             "urn:ietf:wg:oauth:2.0:oob",
                             new Scope(Scope.Name.ALL),
                             "https://probos.app"
                     ).execute();
+
                     System.out.println("instance=" + registration.getInstanceName());
                     Log.d("LoginActivity",registration.getInstanceName());
                     System.out.println("client_id=" + registration.getClientId());
                     Log.d("LoginActivity",registration.getClientId());
+                    clientId = registration.getClientId();
                     System.out.println("client_secret=" + registration.getClientSecret());
                     Log.d("LoginActivity",registration.getClientSecret());
-
+                    clientSecret = registration.getClientSecret();
                 } catch (Mastodon4jRequestException e) {
                     int statusCode = e.getResponse().code();
                     // error handling.
                 }
+
+                String clientId = registration.getClientId();
+                String url = apps.getOAuthUrl(clientId, new Scope(Scope.Name.ALL),"urn:ietf:wg:oauth:2.0:oob");
+
+                // url like bellow
+                // https://:instance_name/oauth/authorize?client_id=:client_id&redirect_uri=:redirect_uri&response_type=code&scope=read
+                // open url and OAuth login and get auth code
+                Intent intent = new Intent(InstanceChoiceActivity.this, WebViewActivity.class);
+                intent.putExtra("url", url);
+                startActivity(intent);
+
+// 	accessToken needs to be saved.
+
             }
         });
 
@@ -194,44 +246,69 @@ public class InstanceChoiceActivity extends AppCompatActivity implements LoaderC
 
         // Reset errors.
         mEmailView.setError(null);
-        mPasswordView.setError(null);
+        //mPasswordView.setError(null);
 
         // Store values at the time of the login attempt.
         String email = mEmailView.getText().toString();
-        String password = mPasswordView.getText().toString();
+        //String password = mPasswordView.getText().toString();
 
         boolean cancel = false;
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+      /*  if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
             mPasswordView.setError(getString(R.string.error_invalid_password));
             focusView = mPasswordView;
             cancel = true;
-        }
+        }*/
 
         // Check for a valid email address.
         if (TextUtils.isEmpty(email)) {
             mEmailView.setError(getString(R.string.error_field_required));
             focusView = mEmailView;
             cancel = true;
-        } else if (!isEmailValid(email)) {
+        } /*else if (!isEmailValid(email)) {
             mEmailView.setError(getString(R.string.error_invalid_email));
             focusView = mEmailView;
             cancel = true;
-        }
+        } */
 
         if (cancel) {
             // There was an error; don't attempt login and focus the first
             // form field with an error.
             focusView.requestFocus();
-        } else {
+        } /*else {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
-        }
+        }*/
+    }
+
+    private void actualSignIn() throws Mastodon4jRequestException {
+        TextView authField = findViewById(R.id.auth_text);
+        authCode = authField.getText().toString();
+        MastodonRequest<AccessToken> accessTokenMastodonRequest = apps.getAccessToken(clientId, clientSecret, "urn:ietf:wg:oauth:2.0:oob", authCode, "authorization_code");
+
+        Thread finalAuthThr = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    AccessToken accessToken = accessTokenMastodonRequest.execute();
+                    String accessTokenStr = accessToken.getAccessToken();
+                    authClient = new MastodonClient.Builder(instanceName, new OkHttpClient.Builder(), new Gson()).accessToken(accessTokenStr).build();
+                    Timelines timelines = new Timelines(authClient);
+                    Pageable<Status> statuses = timelines.getHome(new Range()).execute();
+                    List<Status> statusList = statuses.getPart();
+                    Log.d("pleasegodwork", statusList.get(0).getAccount().getDisplayName());
+                } catch (Mastodon4jRequestException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        finalAuthThr.start();
     }
 
     private boolean isEmailValid(String email) {
@@ -338,7 +415,7 @@ public class InstanceChoiceActivity extends AppCompatActivity implements LoaderC
      * Represents an asynchronous login/registration task used to authenticate
      * the user.
      */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+  /*  public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 
         private final String mEmail;
         private final String mPassword;
@@ -389,6 +466,22 @@ public class InstanceChoiceActivity extends AppCompatActivity implements LoaderC
             mAuthTask = null;
             showProgress(false);
         }
+
     }
+    */
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleOAuthResponse(intent);
+    }
+
+    private void handleOAuthResponse(Intent intent) {
+        Uri uri = intent.getData();
+        if (uri != null && uri.toString().startsWith("https://" + instanceName)) {
+            authCode = uri.getQueryParameter("code");
+        }
+    }
+
 }
 
---
+++ app/src/main/java/app/probos/probos/WebViewActivity.java
@@ -0,0 +1,27 @@
+package app.probos.probos;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends Activity {
+
+    private WebView webView;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        Intent i = getIntent();
+        String url= i.getStringExtra("url");
+        webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+
+    }
+
+}
