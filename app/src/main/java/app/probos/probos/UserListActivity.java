bdf3db5c1ae216c207bcb5e3481c213e42215fee
mjw46368 <mjw46368@gmail.com>
2019-02-13 21:25:10 -0500

    Create initial user list layout to be refined later

---
+++ app/src/main/java/app/probos/probos/UserListActivity.java
@@ -0,0 +1,22 @@
+package app.probos.probos;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class UserListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+}
