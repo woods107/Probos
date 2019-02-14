f297abf8fc9715762cb66838e416b152acfd20c3
mjw46368 <mjw46368@gmail.com>
2019-02-13 21:28:03 -0500

    Create initial user list layout to be refined later

--- app/src/main/java/app/probos/probos/UserListActivity.java
+++ app/src/main/java/app/probos/probos/UserListActivity.java
@@ -15,6 +15,8 @@ public class UserListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_list);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // This line here should enable a back button on the action bar
+        // Once the UI is more developed, it will be useful to have
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
